38929241dbed8ac230c1c333a5026a454b1ddd36
Pierre-Yves JEZEGOU <pierre-yves.jezegou@ensta-bretagne.org>
2021-10-15 09:18:10 +0200

    Added printing of WAV file metadata

--- 01_PCM_PDM_Toolbox/wav2plot.py
+++ 01_PCM_PDM_Toolbox/wav2plot.py
@@ -1,5 +1,6 @@
 import argparse
 import wave
+import pathlib
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,8 +12,6 @@ def main():
 
     args = arg_parser.parse_args()
 
-
-
     Ts = []
     fs = 0
 
@@ -22,9 +21,11 @@ def main():
             "title": file
         }
         wav = wave.open(file, 'rb')
-        fs = wav.getframerate()
+        metadata = wav.getparams()
+        fs = metadata[2]
         frames = wav.readframes(wav.getnframes())
         sig = np.frombuffer(frames, dtype='int16')
+        file_dict["size_ok"] = (pathlib.Path(file).stat().st_size - 44) == (2 * sig.shape[0])
         file_dict["signal"] = sig
         file_dict["n"] = file_dict["signal"].shape[0]
         T = file_dict["n"] / fs
@@ -36,7 +37,25 @@ def main():
         frq = np.linspace(0, fs / 2, int(file_dict['n'] / 2))
         file_dict["fft"] = fft
         file_dict["freq_vector"] = frq
-
+        fft_max_idx = np.argmax(fft)
+        metadata = {
+            "Channels           ": metadata[0],
+            "Sample Width       ": "{} bits".format(metadata[1] * 8),
+            "Sampling Frequency ": "{:.1f} kHz".format(metadata[2] / 1000),
+            "WAV sample count   ": metadata[3],
+            "Duration           ": "{:.2f} s".format(metadata[3] / metadata[2]),
+            "Compression type   ": metadata[4],
+            "Compression name   ": metadata[5],
+            "Size Coherent      ": "Yes" if file_dict["size_ok"] else "No",
+            "Signal Minimum     ": np.min(sig),
+            "Signal Maximum     ": np.max(sig),
+            "Signal Average     ": "{:.2f}".format(np.average(sig)),
+            "FFT Max Frequency  ": "{:.2f} kHz".format(frq[fft_max_idx] / 1000),
+            "FFT Max Value      ": "{:.2f} dB".format(20 * np.log10(np.abs(fft[fft_max_idx])))
+        }
+        print("File: {}".format(file))
+        for key, item in metadata.items():
+            print("\t{} : {}".format(key, item))
         wav_files.append(file_dict)
 
     fig, axs = plt.subplots(2, 1, constrained_layout=True)
@@ -44,14 +63,8 @@ def main():
 
     for data in wav_files:
         axs[0].plot(data["time_vector"], data["signal"], label=data["title"])
-        # plt.plot(t, np.ones((data[0].shape[0],)) * np.average(data[0]), label="avg: {}".format(data[1]))
-        # print("Avg: \"{}\" = {}".format(data["title"], np.average(data["signal"])))
-    # axs[0].plot([0, 3], [0, 0], label="DAC min")
-    # axs[0].plot([0, 3], [0x7FF, 0x7FF], label="DAC half")
-    # axs[0].plot([0, 3], [3, 3], label="DAC max")
     axs[0].set_title("Time Domain")
     axs[0].set_xlim((0, max(Ts)))
-    # axs[0].set_ylim((0, 3))
     axs[0].set_xlabel("Time (s)")
     axs[0].set_ylabel("Amplitude")
     axs[0].legend()
